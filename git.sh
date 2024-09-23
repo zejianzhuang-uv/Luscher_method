b4d65c3bf9e85ed3e06721a1b429d5c637298475
Zejian Zhuang <1429385099@qq.com>
2024-09-23 10:58:25 +0200

    23-09-2024

---
+++ git.sh
@@ -0,0 +1,7 @@
+# echo "# Luscher_method" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/zejianzhuang-uv/Luscher_method.git
+git push -u origin main
