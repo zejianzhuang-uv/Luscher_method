78ab3f162b913d99b2f95d0f62ff2df62ce8ff10
Zejian Zhuang <1429385099@qq.com>
2024-09-25 15:28:56 +0200

    25-09-2024

--- 1D_energy_level.cpp
+++ 1D_energy_level.cpp
@@ -6,50 +6,83 @@
 #include <bisection.cpp>
 using namespace std;
 
-
-
-
-double energy_level(double ka, double n){
-    return ka * sin(ka * n / 2.) - 5. * cos(ka) * cos(ka * (n / 2. - 1.) );
+double energy_level(double ka, double n)
+{
+    return ka * sin(ka * n / 2.) - 5. * cos(ka) * cos(ka * (n / 2. - 1.));
 }
 
-struct my_f_params {double n;};
-double find_energy_levels(double ka, void * p) {
-    struct my_f_params * params = (struct my_f_params *)p;
-    double n = (params -> n);
+struct my_f_params
+{
+    double n;
+};
+double find_energy_levels(double ka, void *p)
+{
+    struct my_f_params *params = (struct my_f_params *)p;
+    double n = (params->n);
     return energy_level(ka, n);
 }
 
-int main(){
+int main()
+{
     // struct my_f_params params = {5e0};
     vector<double> rv;
+    int pole_num = 10;
     gsl_function F;
     F.function = &find_energy_levels;
     // F.params = &params;
     double init_x[2] = {0e0, 10e0};
-    // rv  = find_roots(F, init_x, 0.01, 10);
-    // for (double r : rv)
-    // {
-    //     cout << r << endl;
-    // };
-    ofstream ff("./energy_level.dat");
+
+    ofstream ff("./energy_level.csv");
     ff.precision(17);
+    for (int i = 0; i < pole_num; i++)
+    {
+        ff << "x" << i+1;
+        if (i != pole_num - 1)
+        {
+            ff << ",";
+        }
+        else
+        {
+            ff << "\n";
+        }
+    }
+
     double n0 = 2e0;
     while (n0 <= 10e0)
     {
         struct my_f_params params = {n0};
         F.params = &params;
-        rv = find_roots(F, init_x, 0.01, 10);
+        rv = find_roots(F, init_x, 0.01, pole_num);
 
-        for (size_t i = 0; i < rv.size(); ++i) {
-        ff << rv[i];
-        if (i != rv.size() - 1) { // 如果不是最后一个元素，输出逗号
-            ff << ",";
-        };}
-        ff << endl;
+        for (int i = 0; i < pole_num; ++i)
+        {   
+            if (i < rv.size())
+            {
+                ff << rv[i];
+                if (i != pole_num - 1)
+                {
+                    ff << ",";
+                }
+                else
+                {
+                    ff << "\n";
+                }
+            }
+            else
+            {
+                ff << "NaN";
+                if (i != pole_num - 1)
+                {
+                    ff << ",";
+                }
+                else
+                {
+                    ff << "\n";
+                }
+            }
+            
+        }
         n0 += 0.01;
     }
     ff.close();
 }
-
-
--- run.sh
+++ run.sh
@@ -1,16 +1,9 @@
 #!/bin/bash
 
 # 编译 C++ 文件
-g++ 1D_energy_level.cpp -o a -lgsl -lgslcblas -std=c++20
+clang++ 1D_energy_level.cpp -o a -lgsl -lgslcblas -std=c++20
 
-# 检查编译是否成功
-if [ $? -eq 0 ]; then
-    echo "Compilation successful. Executing the program..."
-    
-    # 计算程序的执行时间
-    time ./a  # 使用不带选项的 time 命令
-else
-    echo "Compilation failed."
-fi
+
+time ./a
 
 
